9e7a7e581c93377687f5f62770ad0359cdf42093
Anumjawaid <anumjawaid99@gmail.com>
2022-05-11 12:48:51 +0500

    final tweaks

--- src/Components/sidebar.js
+++ src/Components/sidebar.js
@@ -5,21 +5,37 @@ import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/material/ListItemText';
-import MailIcon from '@mui/material/ListItemText';
 import logo from '../UI/logo.png'
-
+import AppsIcon from '@material-ui/icons/Apps';
+import WatchLaterOutlinedIcon from '@material-ui/icons/WatchLaterOutlined';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 const drawerWidth = 290;
 
 export default function SideBar() {
+  const listItems=[
+    {text:"Dashboard",
+     icon:<AppsIcon/>,
+     path:"/dashboard"},
+
+     {text:"Book Appointments",
+     icon:<WatchLaterOutlinedIcon/>,
+     path:"/bookappointment"},
+
+     {text:"Account Settings",
+     icon:<SettingsIcon/>,
+     path:"/accountsettings"},
+
+     {text:"SignOut",
+     icon:<ExitToAppIcon/>,
+     path:"/"},
+  ]
   return (
-    // <Box sx={{ display: 'flex' }}>
     <React.Fragment>
       <CssBaseline />
       <AppBar
@@ -42,27 +58,20 @@ export default function SideBar() {
       >
           {/* If logo then here */}
           <img src={logo} />
-        {/* <Toolbar />
-        <Divider /> */}
+       
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          {listItems.map((text, index) => (
+            <ListItem button key={text.text} style={{color:' #62C5C4',fontSize:'18px'}}>
+              <ListItemIcon style={{color:' #62C5C4',fontSize:'18px'}}>
+                {text.icon}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={text.text} />
             </ListItem>
           ))}
         </List>
        
       </Drawer>
       </React.Fragment>
-    //   <Box
-    //     component="main"
-    //     sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-    //   >
-        
-    //   </Box>
-    // </Box>
+ 
   );
 }
